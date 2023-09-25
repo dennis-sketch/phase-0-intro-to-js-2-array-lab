8dc5c5b28563af9c80a179864391d5524d89a5fb
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-09-25 12:53:46 +0300

    Completed with assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push ("Ralph")
+} 
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+}
+
+function appendCat(){
+   const newCats = [...cats,"Broom"]
+    return newCats
+}
+
+function prependCat(){
+   const newCats = ["Arnold", ...cats]
+    return newCats
+}
+
+const removeLastCat=()=>{
+   const newCats= cats.slice(0,-1);
+    return newCats
+}
+
+const removeFirstCat=()=>{
+   const newCats= cats.slice (1);
+   return newCats
+}
